c82302dffe564dc9afc465a9b62c1cf07d4f5f62
weldoy <132596720+weldoy@users.noreply.github.com>
2025-02-13 19:12:06 +0300

    Add files via upload

---
+++ 19 tasks/19_2.py
@@ -0,0 +1,10 @@
+def f(x, y, p):
+    if x + y >= 82 or p > 3:
+        return p == 3 and x + y >= 82
+    return f(x+1, y, p+1) or f(x*4, y, p+1) or f(x, y+1, p+1) or f(x, y*4, p+1)
+
+
+for s in range(1, 77+1):
+    if f(4, s, 1):
+        print(s)
+        break
