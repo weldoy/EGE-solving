c1269d99ce6134503d8f4ce4677061268ab205cc
weldoy <goshakra@gmail.com>
2025-03-20 10:33:32 +0300

    fix

--- 19-20-21/task 1.py
+++ 19-20-21/task 1.py
@@ -6,7 +6,7 @@ def f(x: int, cnt: int) -> int:
 
     if cnt % 2 > 0:
         return any([f(x + 3, cnt - 1), f(x + 6, cnt - 1), f(x * 3, cnt - 1)])
-    return all([f(x + 3, cnt - 1), f(x + 6, cnt - 1), f(x * 3, cnt - 1)])
+    return any([f(x + 3, cnt - 1), f(x + 6, cnt - 1), f(x * 3, cnt - 1)])
 
 
 def f2(x: int, cnt: int) -> int:
@@ -16,10 +16,10 @@ def f2(x: int, cnt: int) -> int:
         return False
 
     if cnt % 2 > 0:
-        return any([f(x + 3, cnt - 1), f(x + 6, cnt - 1), f(x * 3, cnt - 1)])
-    return any([f(x + 3, cnt - 1), f(x + 6, cnt - 1), f(x * 3, cnt - 1)])
+        return any([f2(x + 3, cnt - 1), f2(x + 6, cnt - 1), f2(x * 3, cnt - 1)])
+    return all([f2(x + 3, cnt - 1), f2(x + 6, cnt - 1), f2(x * 3, cnt - 1)])
 
 
 print(19, [s for s in range(1, 132) if f(s, 2)])
 print(20, [s for s in range(1, 132) if f2(s, 3) and not f2(s, 1)])
-print(21, [s for s in range(1, 132) if f2(s, 4) and f2(s, 2)])
+print(21, [s for s in range(1, 132) if f2(s, 4) and not f2(s, 2)])
