26ba0b038f15b2d53e7a615703c84af7211393e5
weldoy <goshakra@gmail.com>
2025-03-27 19:50:00 +0300

    13 tutor

--- 13 tasks/13_tutor.py
+++ 13 tasks/13_tutor.py
@@ -1,6 +1,9 @@
 from ipaddress import *
 
-net = ip_network('227.116.246.2/23', 0)  # Создание своего адреса сети
-addr = ip_address('227.116.246.2')
+net = ip_network('227.116.246.2/23', 0)  # Адрес узла
+addr = ip_address('227.116.246.2')  # Адрес сети
 iface = ip_interface('227.116.246.2/23')
-print(net, addr, iface.network)  # iface.netword выдаст адрес сети, в которой сидит интерфейс
+print(net, addr, iface.network)  # iface.network выдаст адрес сети, в которой сидит интерфейс
+print(net.network_address)  # Адрес сети выведет
+print(net.num_addresses)  # Кол-во адресов
+print(net.netmask)  # Маска сети
